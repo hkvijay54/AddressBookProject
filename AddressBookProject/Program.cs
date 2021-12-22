1211066e3a288758bc3cc600d40fa272b3fb9d8a
hkvijay54 <hkvijay54@gmail.com>
2021-12-23 02:44:25 +0530

    add contact

--- AddressBookProject/Program.cs
+++ AddressBookProject/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-// Calculate Wages till a condition of total working hours or
-// days is reached for a month
+// Ability to create a Contacts in Address Book with first and last names,
+// address,city, state, zip, phone number andemail...
 
 class AddressBookMain
 {
-    public void Welcome()
+    public string firstName, lastName,address,city,state,email;
+    public double phone, zip;
+
+    public AddressBookMain(string a, string b, string c, string d,string e,string f,double ph,double z)
     {
-        Console.WriteLine(" Wolcome to Address Book Program ");
+        this.firstName = a;
+        this.lastName = b;
+        this.address = c;
+        this.city = d;
+        this.state = e;
+        this.email = f;
+        this.phone = ph;
+        this.zip = z;
+
+    }
+    public void Display()
+    {
+        Console.WriteLine();
+        Console.WriteLine("==============AddressBook===============");
+        Console.WriteLine("Name : " + firstName+" "+lastName);
+        Console.WriteLine("Address : " + address);
+        Console.WriteLine("\t  " + city);
+        Console.WriteLine("\t  " + state);
+        Console.WriteLine("Email : " + email);
+        Console.WriteLine("Mobile : " + phone);
+        Console.WriteLine("Zip_Code : " + zip);
     }
 }
 
@@ -15,7 +38,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        AddressBookMain a = new AddressBookMain();
-        a.Welcome();
+        Console.WriteLine("==============Add Contact===============");
+        Console.Write("Enter First Name:");
+        string fn=Console.ReadLine();
+        Console.Write("Enter Last Name:");
+        string ln = Console.ReadLine();
+        Console.Write("Enter Address :");
+        string adr = Console.ReadLine();
+        Console.Write("Enter City:");
+        string cit = Console.ReadLine();
+        Console.Write("Enter State:");
+        string st = Console.ReadLine();
+        Console.Write("Enter Email:");
+        string em = Console.ReadLine();
+        Console.Write("Enter Contact Number:");
+        double mob = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter Zip Code:");
+        double zc = Convert.ToDouble(Console.ReadLine());
+
+        AddressBookMain res = new AddressBookMain(fn,ln,adr,cit,st,em,mob,zc);
+        res.Display();
     }
 }

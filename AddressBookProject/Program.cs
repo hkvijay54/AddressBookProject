477fbe63e277a26d3fea90ed6315f129c70034df
hkvijay54 <hkvijay54@gmail.com>
2021-12-23 01:53:05 +0530

    Welcome

---
+++ AddressBookProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// Calculate Wages till a condition of total working hours or
+// days is reached for a month
+
+class AddressBookMain
+{
+    public void Welcome()
+    {
+        Console.WriteLine(" Wolcome to Address Book Program ");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AddressBookMain a = new AddressBookMain();
+        a.Welcome();
+    }
+}
